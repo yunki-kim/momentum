8364e05002a66972a0c3d6d2891e9b5fe8cfdbeb
yunki-kim <skool109@naver.com>
2022-08-12 15:33:41 +0900

    Rename file

--- js/login.js
+++ js/login.js
@@ -1,3 +1,5 @@
+const loginContainer = document.querySelector("#login-container");
+const mainContainer = document.querySelector("#main-container");
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
@@ -13,15 +15,21 @@ function onLoginSubmit(event) {
   paintGreetings(username);
 }
 
+function onLogoutSubmit(event) {
+  event.preventDefault();
+  localStorage.removeItem(USERNAME_KEY);
+  localStorage.removeItem("todos");
+}
+
 function paintGreetings(username) {
   greeting.innerText = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+  loginContainer.classList.add(HIDDEN_CLASSNAME);
+  // mainContainer.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
-  loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
   paintGreetings(savedUsername);
